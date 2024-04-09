6e8c936d6fc6318ed2e09f514f141ae040ad34fe
unknown <yulian@avrelinex.com>
2024-04-09 21:23:34 +0200

    edit page ui fix

--- src/app/templates/edit/[id]/page.tsx
+++ src/app/templates/edit/[id]/page.tsx
@@ -7,6 +7,7 @@ import { useQuery } from "@tanstack/react-query";
 import { TemplateDto } from "@/app/lib/dto/Template.dto";
 import { TemplateForm } from "../../components/TemplateForm";
 import { UpdateTemplateDto } from "@/app/lib/dto/UpdateTemplate.dto";
+import { Box } from "@mui/material";
 
 export default function Page({ params }: { params: { id: string } }) {
   const apiClient = ApiClient.getInstance();
@@ -31,12 +32,14 @@ export default function Page({ params }: { params: { id: string } }) {
   if (error) return <div>{error.message}</div>;
 
   return (
-    <TemplateForm
-      defaultValues={data}
-      header="Edit Template"
-      successMessage="Template updated successfully"
-      errorMessage="Failed to update template"
-      submitFn={onSubmit}
-    />
+    <Box sx={{ display: "flex", justifyContent: "center" }}>
+      <TemplateForm
+        defaultValues={data}
+        header="Edit Template"
+        successMessage="Template updated successfully"
+        errorMessage="Failed to update template"
+        submitFn={onSubmit}
+      />
+    </Box>
   );
 }
